ef19887c2de3979275b94398b03f620353ac8d37
palash sarkar <spalash521@gmail.com>
2021-08-27 10:09:02 +0300

    added new addation

--- java2.js
+++ java2.js
@@ -1,3 +1,33 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json));
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
+
+
+
+function loaduser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUser(data));
+
+};
+function loadpost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data));
+};
+// loadpost() // auto called 
+function displayUser(data) {
+    const ul = document.getElementById('users');
+    // console.log(data);
+    for (const user of data) {
+        console.log(user.name)
+        // console.log(user.name);// just check
+        const li = document.createElement('li');
+        li.innerText = `name: ${user.name} emails : ${user.email}`;
+        ul.appendChild(li);
+
+
+    };
+}
