39a8070bfef47074965a6c3ecf6b3aa6b2eebd9f
palash sarkar <spalash521@gmail.com>
2021-08-27 10:56:16 +0300

    finished

--- postjava.js
+++ postjava.js
@@ -1,1 +1,26 @@
-console.log('how us it')
+function loadpost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => displayposts(data))
+}
+
+loadpost();
+
+function displayposts(posts) {
+    const postCantaine = document.getElementById('posts')
+    // console.log(posts);// just check it working or not
+    for (const post of posts) {
+
+        const div = document.createElement('div');
+        div.classList.add('post')
+        div.innerHTML = `
+        <h3> ${post.title} </h3>
+        <p> ${post.body}</p>
+        `;
+        postCantaine.appendChild(div)
+        console.log(post.title);
+
+    }
+
+
+}
